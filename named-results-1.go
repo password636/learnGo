2347e3765e19482f3f9cc71758ac14326b6fcafb
password636 <lizongliang@hotmail.com>
2017-12-06 05:46:59 -0500

    sym1

---
+++ named-results-1.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+/*
+func split(sum int) (x, y int) {
+	//x = sum *4 / 9
+	//y = sum - x
+	return
+}
+*/
+
+func split(sum int) (x, y int) {
+	x = 0
+	y = 0
+	//x = sum *4 / 9
+	//y = sum - x
+	return x, y
+}
+
+func main() {
+	fmt.Println(split(17))
+}
---
+++ type-conversions-1.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+	//"math"
+)
+
+func main() {
+/*
+	var i int = 42
+	var f float64 = float64(i)
+	var u uint = uint(f)
+*/
+	i := 42 
+	f := float64(i)
+	u := uint(f)
+	fmt.Println(i, f, u)
+}
