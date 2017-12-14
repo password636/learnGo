34810e3716b1629f0f26cc35c40ab97944d96968
password636 <lizongliang@hotmail.com>
2017-12-14 06:27:27 -0500

    s

---
+++ semicolon.go
@@ -0,0 +1,20 @@
+package main
+import ("fmt")
+
+var i = -1
+
+func main() {
+	//if i > 0 {fmt.Println("positive");} else {fmt.Println("negative");}
+/*
+	if i > 0 {fmt.Println("positive")
+	} else {fmt.Println("negative")
+	}
+*/
+/*
+	if i > 0 {
+    	fmt.Println("positive")
+	} else {
+    	fmt.Println("negative")
+	}
+*/
+}
