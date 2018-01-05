77833c41ebc4c72a9aa99fa8124cb9407ffda1ee
password636 <lizongliang@hotmail.com>
2018-01-05 05:25:30 -0500

    sym1

--- switch.c
+++ switch.c
@@ -7,16 +7,17 @@ int ten()
 
 int main() 
 {
-	int c = 10;
+	int c = 1;
 	switch (c) {
-		//case ten() : 
-		//case "10":
-		case 2 + 8 : 
-			printf("matched\n");
-			printf("matched again\n");
-			break;
-		case 11:
+		case c + 1:
+			printf("It's 1.\n");
+		case 2: 
+			printf("It's 2.\n");
+		case 3: 
+			printf("It's 3.\n");
+		case 4: 
+			printf("It's 4.\n");
 		default:
-			printf("no matched\n");
+			printf("no match!\n");
 	}
 }
